6bab32f1b6dd5bac54277d1122108a2388bf2e0f
Wen-Tien Chang <ihower@gmail.com>
2016-04-26 00:25:35 +0800

    Init basic front-end project

---
+++ entry.js
@@ -0,0 +1,4 @@
+var App = require("./js/app.js");
+window.$ = require('jquery');
+
+App.init();
---
+++ js/app.js
@@ -0,0 +1,12 @@
+var app = {
+
+  init: function(){
+
+    $(document).ready(function(){
+      $("#content").html("test");
+    })
+
+  }
+}
+
+module.exports = app;
